08d32048ba11366656feb437fc42c67388bf16ff
Ryan Moore <s-rmoore1@unl.edu>
2018-02-09 09:51:46 -0600

    Changed scale length filter to greater than or equal to 1

--- fish-analysis.R
+++ fish-analysis.R
@@ -6,4 +6,4 @@ fish_data <- read_csv("Gaeta_etal_CLC_data.csv")
 fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small")) -> fish_data_cat
 
-fish_data_cat %>% filter(scalelength > 1) -> fish_data_cat_modi
+fish_data_cat %>% filter(scalelength >= 1) -> fish_data_cat
